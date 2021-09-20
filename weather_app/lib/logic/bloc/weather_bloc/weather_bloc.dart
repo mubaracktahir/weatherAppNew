2fd22f06e3de8d760c1d7b0a3e56fa2c69015c3f
usmanabdulmajid <abstractmajid@gmail.com>
2021-09-20 06:21:42 -0700

    add new launch icon

--- weather_app/lib/logic/bloc/weather_bloc/weather_bloc.dart
+++ weather_app/lib/logic/bloc/weather_bloc/weather_bloc.dart
@@ -25,49 +25,11 @@ class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
       weatherList = await weatherRepository.getWeatherData();
     }
 
-    var currentHour = DateTime.now();
-
-    List<Weather> temp = [];
-
-    for (var i = 0; i < weatherList.length - 1; i++) {
-      //DateTime dateTimeC = DateTime()
-      if (weatherList[i].dateTime.difference(currentHour).inHours < 3 &&
-          weatherList[i].dateTime.difference(currentHour).inHours > 1) {
-        temp.add(weatherList[i]);
-      }
-    }
-
-    weatherList = temp;
     print('omo ${weatherList.length}');
-    yield WeatherLoaded(weatherData: weatherList);
+    yield WeatherLoaded(weatherData: _refactorWeatherData(weatherList));
   }
 
   List<Weather> _refactorWeatherData(List<Weather> weatherList) {
-    List<Weather> tempList = [];
-    for (var i = 0; i < weatherList.length - 1; i++) {
-      var hour = weatherList[i].dateTime.hour;
-
-      if (hour >= 0 && hour < 3) {
-        tempList.add(weatherList[i]);
-      } else if (hour >= 3 && hour < 6) {
-        tempList.add(weatherList[i]);
-      } else if (hour >= 6 && hour < 9) {
-        tempList.add(weatherList[i]);
-      } else if (hour >= 9 && hour < 12) {
-        tempList.add(weatherList[i]);
-      } else if (hour >= 12 && hour < 15) {
-        tempList.add(weatherList[i]);
-      } else if (hour >= 15 && hour < 18) {
-        tempList.add(weatherList[i]);
-      } else if (hour >= 18 && hour < 21) {
-        tempList.add(weatherList[i]);
-      } else {
-        tempList.add(weatherList[i]);
-      }
-    }
-
-    weatherList = tempList;
-
     List<Weather> temp = [];
     for (var i = 0; i < weatherList.length - 1; i++) {
       if (weatherList[i].day != weatherList[i + 1].day) {
